b5c994a9dd651c95d4be90b6e0fc66470b28fef9
Gitman <Gitman@Komputer>
2025-07-11 19:48:51 +0200

    chunk css into small file and use sass

---
+++ hero_cards/hero_cards.js
@@ -0,0 +1,50 @@
+export const firstCardContent = ` <h2 class="hero-wrapper-content-wrapper-description-header">
+                Odkryj Varilux Physio extensee
+              </h2>
+              <p class="hero-wrapper-content-wrapper-description-para">
+                Soczewka progresywna, która przewiduje zmiany rozmiaru źrenicy wciągu dnia. Wspomagane przez sztuczną inteligencję ioparte na dynamice źrenic soczewki Varilux® Physio® extensee™ optymalizują budowę soczewki wzależności od zmian rozmiaru źrenicy, zapewniając doskonałe widzenie wkażdym świetle.
+              </p>
+              <button class="hero-wrapper-content-wrapper-description-button">Odkryj</button>`;
+export const secondCardContent = ` <h2 class="hero-wrapper-content-wrapper-description-header">
+                Evolving vision
+              </h2>
+              <p class="hero-wrapper-content-wrapper-description-para">
+               W Essilor® wierzymy, że wzrok to nasz najcenniejszy zmysł, a pełne wykorzystanie jego potencjału może odmienić życie. Odkryj, jak nasze nieustanne innowacje wzmacniają i chronią Twój wzrok.
+              </p>
+              <button class="hero-wrapper-content-wrapper-description-button">Odkryj</button>`;
+export const thirdCardContent = ` <h2 class="hero-wrapper-content-wrapper-description-header">
+                Odkryj Transitions GEN S
+              </h2>
+              <p class="hero-wrapper-content-wrapper-description-para">
+                Oto Transitions® GEN S™​: nowy, ponadprzeciętny, oferujący dynamiczne i wyraźne widzenie, współgrający z ciągle zmieniającym się tempem życia standard dla soczewek, który pokochasz.
+              </p>
+              <button class="hero-wrapper-content-wrapper-description-button">Odkryj</button>`;
+export const fourthCardContent = ` <h2 class="hero-wrapper-content-wrapper-description-header">
+               Niezwykłe soczewki poprawiające jakość widzenia bez wysiłku
+              </h2>
+              <p class="hero-wrapper-content-wrapper-description-para">
+                Daj się oczarować przygodzie Persol x Varilux®, w której przejrzystość staje się oczywista. Ciesz się płynnymi przejściami z bliska na odległość, a wszystko to w kultowym stylu, który idealnie pasuje do Twojej wizji.
+              </p>
+              <button class="hero-wrapper-content-wrapper-description-button">Odkryj</button>`;
+export const fifthCardContent = ` <h2 class="hero-wrapper-content-wrapper-description-header">
+                Nowa generacja soczewek Varilux XR series
+              </h2>
+              <p class="hero-wrapper-content-wrapper-description-para">
+                To soczewki progresywne, które perfekcyjnie odzwierciedlają ruchy Twoich oczu. Dzięki zaawansowanej technologii sztucznej inteligencji, soczewki Varilux® XR series™ są w stanie przewidzieć, jak obserwujesz świat, zapewniając natychmiastową ostrość, nawet podczas ruchu oraz płynne przejście od bliży do dali.
+              </p>
+              <button class="hero-wrapper-content-wrapper-description-button">Odkryj</button>`;
+export const sixthCardContent = ` <h2 class="hero-wrapper-content-wrapper-description-header">
+                Essilor Expertse
+              </h2>
+              <p class="hero-wrapper-content-wrapper-description-para">
+                Nie zawsze łatwo jest znaleźć idealną parę okularów. Oprawy muszą nam się podobać, a szkła okularowe – pasować do naszego stylu życia. Dzięki poradom właściwego specjalisty ochrony wzroku cały proces przebiegnie o wiele płynniej.
+              </p>
+              <button class="hero-wrapper-content-wrapper-description-button">Odkryj</button>`;
+export {
+  firstCardContent,
+  secondCardContent,
+  thirdCardContent,
+  fourthCardContent,
+  fifthCardContent,
+  sixthCardContent,
+};
---
+++ script.js
@@ -0,0 +1,11 @@
+import {
+  firstCardContent,
+  secondCardContent,
+  thirdCardContent,
+  fourthCardContent,
+  fifthCardContent,
+  sixthCardContent,
+} from "./hero_cards/hero_cards";
+const description = document.querySelector(
+  ".hero-wrapper-content-wrapper-description"
+);
